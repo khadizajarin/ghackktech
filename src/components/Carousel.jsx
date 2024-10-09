e0dcd69bd9fc3e86103c18f9ac03d7df96618456
khadizajarinroza <khadiza131310@gmail.com>
2024-10-10 03:40:02 +0600

    carousel fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,15 +19,15 @@ const cinzel_decorative = Cinzel_Decorative({
 });
 
 const manhwas = [
-  { title: 'Leviathan', image: 'https://animemangatoon.com/wp-content/uploads/2024/06/pjimage-12-4-e1720973001888-1140x570.jpg' },
-  { title: 'Tower of God', image: 'https://animemangatoon.com/wp-content/uploads/2024/06/season-1-1.jpg' },
-  { title: 'Existence', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/Screenshot-2024-06-25-113722-e1721128847688.jpg' },
-  { title: 'The Breaker', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/650x650_9a375a18096744f1075cfab98ecd25e8f0c2242407633b527cd89760-1-e1721128882154.jpg' },
-  { title: 'The First Hunter', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/Screenshot-2024-06-25-191552-e1721129164159-750x375.jpg' },
-  { title: 'Supernatural Manhwa: Noblesse', image: 'https://animemangatoon.com/wp-content/uploads/2024/06/Screenshot-2024-06-15-154729-e1721129126183-750x375.jpg' },
-  { title: 'Ultimate Outcast', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/manhwa-Greatest-Outcast-e1721129034969-1140x570.jpg' },
-  { title: 'Supernatural Manhwa: Unholy Blood', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/Screenshot-2024-06-25-193948-e1721129000296-750x375.jpg' },
-  { title: 'Dice: The Cube that Changes Everything', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/Screenshot-2024-06-25-194410-e1721128974884-750x375.jpg' },
+  { title: '1. Leviathan', image: 'https://animemangatoon.com/wp-content/uploads/2024/06/pjimage-12-4-e1720973001888-1140x570.jpg' },
+  { title: '2. Tower of God', image: 'https://animemangatoon.com/wp-content/uploads/2024/06/season-1-1.jpg' },
+  { title: '3. Existence', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/Screenshot-2024-06-25-113722-e1721128847688.jpg' },
+  { title: '4. The Breaker', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/650x650_9a375a18096744f1075cfab98ecd25e8f0c2242407633b527cd89760-1-e1721128882154.jpg' },
+  { title: '5. The First Hunter', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/Screenshot-2024-06-25-191552-e1721129164159-750x375.jpg' },
+  { title: '6. Supernatural Manhwa: Noblesse', image: 'https://animemangatoon.com/wp-content/uploads/2024/06/Screenshot-2024-06-15-154729-e1721129126183-750x375.jpg' },
+  { title: '7. Ultimate Outcast', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/manhwa-Greatest-Outcast-e1721129034969-1140x570.jpg' },
+  { title: '8. Supernatural Manhwa: Unholy Blood', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/Screenshot-2024-06-25-193948-e1721129000296-750x375.jpg' },
+  { title: '9. Dice: The Cube that Changes Everything', image: 'https://animemangatoon.com/wp-content/uploads/2024/05/Screenshot-2024-06-25-194410-e1721128974884-750x375.jpg' },
 ];
 
 const Carousel = () => {
@@ -53,14 +53,14 @@ const Carousel = () => {
     <section className="bg-secondary bg-opacity-40 lg:pb-32 pb-16 pt-20 h-full">
       <div className="lg:px-40">
         <h2 className="text-3xl font-bold text-center text-primary pb-10" style={{ fontFamily: cinzel.style.fontFamily, fontWeight: '900' }}>The 10 Best Supernatural Manhwas</h2>
-        <div className="relative w-full overflow-hidden lg:h-[31.25rem] h-[12.5rem]" style={{ }}>
+        <div className="relative w-full overflow-hidden lg:h-[31.25rem] h-[12.5rem] " >
           {manhwas.map((manhwa, index) => (
             <div
               key={index}
-              className={`carousel-item absolute w-full transition-transform duration-3000 h-[31.25rem] pb-20 ${index === current ? 'block' : 'hidden'}`}
+              className={`carousel-item absolute w-full transition-transform duration-3000 ease-in-out h-[31.25rem] pb-20 ${index === current ? 'block' : 'hidden'}`}
               style={{ opacity: index === current ? 1 : 0 }}
             >
-              <Image src={manhwa.image} width="500" height="500" alt={manhwa.title} className="w-full h-full object-cover" />
+              <Image src={manhwa.image} width="500" height="500" alt={manhwa.title} className="w-full h-full object-cover flex transition-transform ease-in-out" />
               <div className="carousel-caption absolute text-center inset-0 flex flex-col items-center justify-center lg:h-[31.25rem] h-[13rem] bg-primary bg-opacity-50">
                 <h5 className="lg:text-3xl text-lg flex justify-center items-center text-secondary px-4" style={{ fontFamily: cinzel_decorative.style.fontFamily, fontWeight: '900' }}>{manhwa.title}</h5>
               </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
-// components/Hero.js
+"use client"
+import Typewriter from 'typewriter-effect';
 import { Cinzel_Decorative,Cinzel } from 'next/font/google'
 
 const cinzel = Cinzel({
@@ -19,7 +20,15 @@ const Hero = () => {
         <div className="hero-overlay bg-primary bg-opacity-50"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-lg">
-            <h1 className="mb-5 text-5xl font-bold animate-bounce text-secondary" style={{ fontFamily: cinzel_decorative.style.fontFamily,  fontWeight: '900' }}>Lore Olympus</h1>
+            <h1 className="mb-5 text-5xl font-bold  text-secondary" style={{ fontFamily: cinzel_decorative.style.fontFamily,  fontWeight: '900' }}>
+              
+            <Typewriter
+              options={{ loop: true, autoStart: true }}
+              onInit={(typewriter) => {
+                typewriter.typeString('Lore Olympus').pauseFor(5000).start();
+              }}
+            />
+              </h1>
             <p className="mb-5 text-accent" style={{ fontFamily: cinzel.style.fontFamily,  fontWeight: '900' }}>
               A modern retelling of the story of Hades and Persephone.
             </p>
