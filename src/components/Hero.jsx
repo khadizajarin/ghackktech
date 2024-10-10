59b57270b8eafd6d17d5df34fd898148f800a17a
khadizajarinroza <khadiza131310@gmail.com>
2024-10-10 23:43:34 +0600

    correction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@
 import Typewriter from 'typewriter-effect';
 import { Cinzel_Decorative,Cinzel } from 'next/font/google'
 
+
 const cinzel = Cinzel({
   subsets: ['latin'],
   display: 'swap',
@@ -19,9 +20,8 @@ const Hero = () => {
       <section className="hero lg:min-h-screen h-[40rem]" style={{ backgroundImage: 'url(https://www.shutterstock.com/image-photo/greek-mythologyinspired-background-169-aspect-260nw-2501167313.jpg)' }}>
         <div className="hero-overlay bg-primary bg-opacity-50"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-lg">
-            <h1 className="mb-5 text-5xl font-bold  text-secondary" style={{ fontFamily: cinzel_decorative.style.fontFamily,  fontWeight: '900' }}>
-              
+          <div className="">
+            <h1 className="mb-5 text-5xl font-bold  text-secondary" style={{ fontFamily: cinzel_decorative.style.fontFamily,  fontWeight: '900' }}> 
             <Typewriter
               options={{ loop: true, autoStart: true }}
               onInit={(typewriter) => {
